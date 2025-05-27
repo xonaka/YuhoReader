655931681ebcf94bb97aa500cd8334adebf3a6b3
Taiki Nonaka <tainona0119@outlook.jp>
2025-05-28 03:52:00 +0900

    fix: ルートパスでトップページを表示するようURL設定を修正

--- seculities/main/urls.py
+++ seculities/main/urls.py
@@ -5,6 +5,7 @@ from django.urls import include, path
 
 
 urlpatterns = [
+    path('', views.top, name='root_top'),
     path('top/', views.top, name='top'),
     path('upload', views.upload, name='upload'),
     path('upload_complete', views.upload_complete, name='upload_complete'),
